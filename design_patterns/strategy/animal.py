bbc05a7270d28d9a0972c8c363b9dd36028c9ec3
Zile Rehman <rehmanz@yahoo.com>
2014-07-05 18:02:58 -0700

    Strategy Pattern using the Flys class as the strategy for various animals.

---
+++ design_patterns/strategy/animal.py
@@ -0,0 +1,65 @@
+'''
+Created on Jul 5, 2014
+
+@author: zrehman
+'''
+import unittest
+
+from flys import Flys, ItFlys, CantFly
+
+class Animal:
+    def __init__(self, name):
+        try:
+            self.name  = name
+        except Exception as e:
+            raise ValueError('AnimalExceptionError - Constructor failed: %s'
+                             %(e))
+    
+    def try_to_fly(self):
+        return self.fly_o.fly()
+    
+    def set_flying_ability(self, fly_o):
+        self.fly_o = fly_o
+
+    """ Public Methods """
+    def get_name(self):
+        return self.name
+
+class Bird(Animal):
+    def __init__(self, name):
+        Animal.__init__(self, name=name)
+        self.fly_o = Flys(context=ItFlys)
+
+class Dog(Animal):
+    def __init__(self, name):
+        Animal.__init__(self, name=name)
+        self.fly_o = Flys(context=CantFly)
+    
+""" Unit Tests """
+class AnimalUnitTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(self):
+        self.bird_o = Bird('Tweety')
+        self.dog_o  = Dog('Hooch')
+    
+    def test_fly(self):
+        self.assertEqual(self.bird_o.try_to_fly(), 'Soaring high!')
+        self.assertEqual(self.dog_o.try_to_fly(), 'Can\'t fly')
+    
+    def test_dog_flies_now(self):
+        self.dog_o.set_flying_ability(Flys(context=ItFlys))
+        self.assertEqual(self.dog_o.try_to_fly(), 'Soaring high!')
+        
+    @classmethod
+    def tearDownClass(self):
+        pass
+
+""" Unit Test Runner """
+def AnimalUnitTestRunner():
+    tests = ['test_fly',
+             'test_dog_flies_now']
+    
+    return unittest.TestSuite(map(AnimalUnitTest, tests))
+
+if __name__ == "__main__":
+    unittest.main(defaultTest='AnimalUnitTestRunner', verbosity=2)
---
+++ design_patterns/strategy/flys.py
@@ -0,0 +1,54 @@
+'''
+Created on Jul 5, 2014
+
+@author: zrehman
+'''
+import unittest
+import types
+
+""" Flys Strategy Class """
+class Flys(object):
+    def __init__(self, context=None):
+        self.action = context()
+    
+    def set_flying_ability(self, context):
+        self.action = context()
+    
+    def fly(self):
+        return self.action.execute()
+
+""" Concrete Strategies """
+class ItFlys(object):
+    def execute(self):
+        return 'Soaring high!'
+
+class CantFly(object):
+    def execute(self):
+        return 'Can\'t fly'
+    
+""" Unit Tests """
+class FlyStrategyUnitTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(self):
+        self.fly_o = Flys(context=ItFlys)
+    
+    def test_fly(self):
+        self.assertEqual(self.fly_o.fly(), 'Soaring high!')
+    
+    def test_set_flying_ability(self):
+        self.fly_o.set_flying_ability(context=CantFly)
+        self.assertEqual(self.fly_o.fly(), 'Can\'t fly')
+    
+    @classmethod
+    def tearDownClass(self):
+        pass
+
+""" Unit Test Runner """
+def FlyStrategyUnitTestRunner():
+    tests = ['test_fly',
+             'test_set_flying_ability']
+    
+    return unittest.TestSuite(map(FlyStrategyUnitTest, tests))
+
+if __name__ == "__main__":
+    unittest.main(defaultTest='FlyStrategyUnitTestRunner', verbosity=2)
